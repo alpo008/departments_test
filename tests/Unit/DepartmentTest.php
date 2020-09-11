aa448b706ffb66a9b42def4ee365d43fbba3e889
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2020-09-11 21:23:43 +0300

    Tests

---
+++ tests/Unit/DepartmentTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Department;
+use App\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Tests\TestCase;
+
+class DepartmentTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    private $departments;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp() :void
+    {
+        parent::setUp();
+
+        $this->departments = Department::all();
+    }
+
+    /**
+     * Testing class App\Department
+     */
+    public function testDepartmentClass()
+    {
+        $rules = Department::rules();
+        $messages = Department::messages();
+        $this->assertClassHasAttribute('fillable', Department::class);
+        $this->assertTrue(is_array($rules));
+        $this->assertCount(3, $rules);
+        $this->assertTrue(is_array($messages));
+        $this->assertCount(6, $messages);
+    }
+
+    /**
+     * Testing App\Department[] Collection
+     */
+    public function testDepartments()
+    {
+        $this->assertTrue($this->departments instanceof Collection);
+        foreach ($this->departments as $department) {
+            $this->assertTrue($department instanceof Department);
+            if ($logo = $department->logo) {
+                $this->assertTrue(is_file(public_path($logo)));
+            }
+            if ($users = $department->users) {
+                $this->assertTrue($users instanceof Collection);
+                foreach ($users as $user) {
+                    $this->assertTrue($user instanceof User);
+                    $this->assertGreaterThan(0, count($user->departments));
+                    $this->assertTrue($user->pivot instanceof Pivot);
+                    $this->assertEquals('department_user', $user->pivot->getTable());
+                }
+            }
+        }
+    }
+}
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Department;
+use App\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    private $user;
+    private $users;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp() :void
+    {
+        parent::setUp();
+
+        $this->user = User::first();
+        $this->users = User::all();
+    }
+
+    /**
+     * Testing class App\User
+     */
+    public function testUserClass()
+    {
+        $rules = User::rules();
+        $messages = User::messages();
+        $this->assertClassHasAttribute('fillable', User::class);
+        $this->assertClassHasAttribute('hidden', User::class);
+        $this->assertClassHasAttribute('casts', User::class);
+        $this->assertTrue(is_array($rules));
+        $this->assertCount(3, $rules);
+        $this->assertTrue(is_array($messages));
+        $this->assertCount(6, $messages);
+    }
+
+    /**
+     * Testing instance of class App\User
+     */
+    public function testUser()
+    {
+        $this->assertTrue($this->user instanceof User);
+        $this->assertTrue($this->users instanceof Collection);
+        $this->assertEquals('Tester', $this->user->name);
+        $this->assertTrue($this->user->departments  instanceof Collection);
+        $this->assertGreaterThan(1, count($this->user->departments));
+
+        foreach ($this->user->departments as $department) {
+            $this->assertTrue($department instanceof Department);
+            $this->assertTrue($department->pivot instanceof Pivot);
+            $this->assertEquals('department_user', $department->pivot->getTable());
+        }
+    }
+
+    /**
+     * Testing App\User[] Collection
+     */
+    public function testUsers()
+    {
+        $this->assertTrue($this->users instanceof Collection);
+        foreach ($this->users as $user) {
+            $this->assertTrue($user instanceof User);
+        }
+    }
+}
